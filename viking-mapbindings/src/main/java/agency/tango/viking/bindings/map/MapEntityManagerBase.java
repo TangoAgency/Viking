98df993634f48177d48990bf79f47811a80fbecf
bezmian <filip@norvox.pl>
2017-01-16 09:47:45 +0100

    Extract common methods from MarkerManager and PathManager to MapEntityManagerBase

--- viking-mapbindings/src/main/java/agency/tango/viking/bindings/map/MapEntityManagerBase.java
+++ viking-mapbindings/src/main/java/agency/tango/viking/bindings/map/MapEntityManagerBase.java
@@ -1,12 +1,125 @@
 package agency.tango.viking.bindings.map;
 
+import android.databinding.ObservableList;
+
+import com.android.annotations.NonNull;
 import com.google.android.gms.maps.GoogleMap;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static agency.tango.viking.bindings.map.CollectionUtils.moveRange;
+
 public abstract class MapEntityManagerBase<T, M> implements IMapEntityManager<T> {
+
+  private final MapResolver mapResolver;
+  private List<M> entities = new ArrayList<>();
+  private ObservableList.OnListChangedCallback<ObservableList<T>> itemsListener;
+
+  MapEntityManagerBase(MapResolver mapResolver) {
+    this.mapResolver = mapResolver;
+
+    itemsListener = new ObservableList.OnListChangedCallback<ObservableList<T>>() {
+      @Override
+      public void onChanged(ObservableList<T> observableList) {
+        mapResolver.resolve(googleMap -> addItems(googleMap, observableList));
+      }
+
+      @Override
+      public void onItemRangeChanged(ObservableList<T> observableList, int fromIndex,
+          int itemCount) {
+        mapResolver.resolve(googleMap -> {
+          for (int i = fromIndex; i < itemCount; i++) {
+            update(entities.get(i), observableList.get(i), googleMap);
+          }
+        });
+      }
+
+      @Override
+      public void onItemRangeInserted(ObservableList<T> observableList, int fromIndex,
+          int itemCount) {
+        mapResolver.resolve(googleMap -> {
+          for (int i = fromIndex; i <= observableList.size() - itemCount; i++) {
+            entities.add(i, create(observableList.get(i), googleMap));
+          }
+        });
+      }
+
+      @Override
+      public void onItemRangeMoved(ObservableList<T> observableList, int fromPosition,
+          int toPosition, int itemCount) {
+        moveRange(entities, fromPosition, toPosition, itemCount);
+      }
+
+      @Override
+      public void onItemRangeRemoved(ObservableList<T> observableList, int fromIndex,
+          int itemCount) {
+        mapResolver.resolve(googleMap -> {
+          for (int i = fromIndex; i < itemCount; i++) {
+            M entity = entities.remove(i);
+            if (entity != null) {
+              remove(entity, googleMap);
+            }
+          }
+        });
+      }
+    };
+  }
+
+  @Override
+  public void add(GoogleMap googleMap, T item) {
+    entities.add(create(item, googleMap));
+  }
+
+  @Override
+  public void addItems(GoogleMap googleMap, Collection<T> items) {
+    if (items instanceof ObservableList) {
+      ((ObservableList<T>) items).addOnListChangedCallback(itemsListener);
+    }
+
+    addItems(items);
+  }
+
+  @Override
+  public void remove(GoogleMap googleMap, @NonNull T item) {
+    for (M entity : entities) {
+      if (compareEntities(entity, item)) {
+        entities.remove(entity);
+        remove(entity, googleMap);
+      }
+    }
+  }
+
+  @Override
+  public void removeAll(GoogleMap googleMap, Collection<T> items) {
+    for (M entity : entities) {
+      for (T item : items) {
+        if (compareEntities(entity, item)) {
+          remove(entity, googleMap);
+        }
+      }
+    }
+  }
+
+  private void addItems(Collection<T> items) {
+    mapResolver.resolve(googleMap -> {
+      for (M entity : entities) {
+        remove(entity, googleMap);
+      }
+      entities.clear();
+
+      for (T item : items) {
+        add(googleMap, item);
+      }
+    });
+  }
 
   abstract M create(T item, GoogleMap googleMap);
 
   abstract void remove(M entity, GoogleMap googleMap);
 
   abstract void update(M entity, T item, GoogleMap googleMap);
+
+  abstract boolean compareEntities(M entity, T item);
 }
--- viking-mapbindings/src/main/java/agency/tango/viking/bindings/map/MarkerManager.java
+++ viking-mapbindings/src/main/java/agency/tango/viking/bindings/map/MarkerManager.java
@@ -1,128 +1,23 @@
 package agency.tango.viking.bindings.map;
 
-import android.databinding.ObservableList;
-import android.support.annotation.NonNull;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import agency.tango.viking.bindings.map.adapters.IMapItemAdapter;
-
-import static agency.tango.viking.bindings.map.CollectionUtils.moveRange;
 
 public class MarkerManager<T> extends MapEntityManagerBase<T, Marker>
     implements IMapEntityManager<T> {
-  private final MapResolver mapResolver;
-  private List<Marker> markers = new ArrayList<>();
+
   private IMapItemAdapter<T> mapItemAdapter;
-  private ObservableList.OnListChangedCallback<ObservableList<T>> itemsListener;
 
   public MarkerManager(MapResolver mapResolver) {
-    this.mapResolver = mapResolver;
-    itemsListener = new ObservableList.OnListChangedCallback<ObservableList<T>>() {
-      @Override
-      public void onChanged(ObservableList<T> observableList) {
-        addItems(observableList);
-      }
-
-      @Override
-      public void onItemRangeChanged(ObservableList<T> observableList, int fromIndex,
-          int itemCount) {
-        mapResolver.resolve(googleMap -> {
-          for (int i = fromIndex; i < itemCount; i++) {
-            update(markers.get(i), observableList.get(i), googleMap);
-          }
-        });
-      }
-
-      @Override
-      public void onItemRangeInserted(ObservableList<T> observableList, int fromIndex,
-          int itemCount) {
-        mapResolver.resolve(googleMap -> {
-          for (int i = fromIndex; i <= observableList.size() - itemCount; i++) {
-            markers.add(i, create(observableList.get(i), googleMap));
-          }
-        });
-      }
-
-      @Override
-      public void onItemRangeMoved(ObservableList<T> observableList, int fromPosition,
-          int toPosition, int itemCount) {
-        moveRange(markers, fromPosition, toPosition, itemCount);
-      }
-
-      @Override
-      public void onItemRangeRemoved(ObservableList<T> observableList, int fromIndex,
-          int itemCount) {
-        mapResolver.resolve(googleMap1 -> {
-          for (int i = fromIndex; i < itemCount; i++) {
-            Marker marker = markers.remove(i);
-            if (marker != null) {
-              marker.remove();
-            }
-          }
-        });
-      }
-    };
-  }
-
-  @Override
-  public void add(GoogleMap googleMap, T item) {
-    this.markers.add(create(item, googleMap));
-  }
-
-  @Override
-  public void addItems(GoogleMap googleMap, Collection<T> items) {
-    if (items instanceof ObservableList) {
-      ((ObservableList<T>) items).addOnListChangedCallback(itemsListener);
-    }
-
-    addItems(items);
-  }
-
-  @Override
-  public void remove(GoogleMap googleMap, @NonNull T item) {
-    for (Marker marker : markers) {
-      if (item.equals(marker.getTag())) {
-        markers.remove(marker);
-        marker.remove();
-      }
-    }
-  }
-
-  @Override
-  public void removeAll(GoogleMap googleMap, Collection<T> items) {
-    for (Marker marker : markers) {
-      for (T item : items) {
-        if (item.equals(marker.getTag())) {
-          markers.remove(marker);
-          marker.remove();
-        }
-      }
-    }
+    super(mapResolver);
   }
 
   public void setItemsAdapter(IMapItemAdapter<T> mapItemAdapter) {
     this.mapItemAdapter = mapItemAdapter;
-  }
-
-  private void addItems(Collection<T> items) {
-    mapResolver.resolve(googleMap -> {
-      for (Marker marker : markers) {
-        marker.remove();
-      }
-      markers.clear();
-
-      for (T item : items) {
-        add(googleMap, item);
-      }
-    });
   }
 
   @Override
@@ -153,8 +48,12 @@ public class MarkerManager<T> extends MapEntityManagerBase<T, Marker>
     }
 
     entity.setTag(item);
-
     entity.setIcon(BitmapDescriptorFactory.fromResource(mapItemAdapter.icon(item)));
     entity.setPosition(mapItemAdapter.position(item));
+  }
+
+  @Override
+  boolean compareEntities(Marker entity, T item) {
+    return item.equals(entity.getTag());
   }
 }

8fe953fdee7ce5cd01fd97ab290be994bd3ad6f1
Radek Piekarz <radoslaw.piekarz@gmail.com>
2017-01-25 15:19:13 +0100

    Fix ExampleInstrumentedTest.

--- viking-mvvm/src/androidTest/java/agency/tango/viking/mvvm/ExampleInstrumentedTest.java
+++ viking-mvvm/src/androidTest/java/agency/tango/viking/mvvm/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("agency.tango.viking.viewmodel.test", appContext.getPackageName());
+    assertEquals("agency.tango.viking.mvvm.test", appContext.getPackageName());
   }
 }

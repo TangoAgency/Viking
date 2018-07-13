52894196c4f5e59ed33b8cb84350d51935051340
Marcin Habzda <marcin.habzda1@gmail.com>
2018-07-13 10:29:02 +0200

    Modify dialog in the example to not have any title

--- example/src/main/java/agency/tango/viking/example/dialog/VikingDialogFragment.java
+++ example/src/main/java/agency/tango/viking/example/dialog/VikingDialogFragment.java
@@ -3,13 +3,16 @@ package agency.tango.viking.example.dialog;
 import agency.tango.viking.annotations.AutoModule;
 import agency.tango.viking.example.R;
 import agency.tango.viking.mvp.DialogFragmentScreen;
+import androidx.fragment.app.DialogFragment;
 
 @AutoModule
 public class VikingDialogFragment extends DialogFragmentScreen<VikingDialogContract.View, VikingDialogPresenter>
     implements VikingDialogContract.View {
 
   public static VikingDialogFragment newInstance() {
-    return new VikingDialogFragment();
+    VikingDialogFragment fragment = new VikingDialogFragment();
+    fragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+    return fragment;
   }
 
   public VikingDialogFragment() {
